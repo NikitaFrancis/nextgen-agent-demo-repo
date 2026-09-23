ef7726d93c3d8a724e10697a71c9802734e36502
Francis Nicole Baños Flores <126989774+NikitaFrancis@users.noreply.github.com>
2026-09-23 17:27:37 -0400

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -2,14 +2,13 @@ namespace DemoApp;
 
 public class Calculator
 {
-    public int Add(int a, int b) => a + b;
+    public int Multiply(int a, int b)
+    {
+        return a + b; // Bug intencional: suma en lugar de multiplicación
+    }
 
-    public int Subtract(int a, int b) => a - b;
-
-    // BUG INTENCIONAL PARA LA DEMO:
-    // La función de división debería usar '/', pero tiene '*'
     public int Divide(int a, int b)
     {
-        return a * b; 
+        return a / b;
     }
 }
